6d0c9e9f05bef17f5c44384011df1ddb44d21993
MichaelBlm <MichaelBlm94@gmail.com>
2023-04-02 09:38:36 -0700

    Refactored and finished project

--- Program.cs
+++ Program.cs
@@ -1,37 +1,91 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+
 namespace HashProject;
 class Program
-{
+{   
+//   public static string GetInput(string[] args)
+//   {
+//     string input = "";
 
-    public static byte[] addByteToArray(byte[] byteArray, byte newByte)
+//     if( args.Length == 1)
+//     {
+//         input = args[0];
+//     }
+//     else
+//     {
+//         Console.WriteLine("Not enough inputs provided.");
+//     }
+//     return input;
+//   }
+ 
+  //https://stackoverflow.com/a/24031467
+  public static string CreateMD5(string input, string salt, int bytesLength)
+  {
+     using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] saltBytes = new byte[] {Convert.ToByte(salt,16)};
+                byte[] saltedInputBytes = new byte[inputBytes.Length + saltBytes.Length];
+                System.Buffer.BlockCopy(inputBytes, 0, saltedInputBytes, 0, inputBytes.Length);
+                System.Buffer.BlockCopy(saltBytes, 0, saltedInputBytes, inputBytes.Length, saltBytes.Length);                                
+                byte[] hashBytes = md5.ComputeHash(saltedInputBytes);
+                byte[] reducedBytes = hashBytes.Take(bytesLength).ToArray();
+                return BitConverter.ToString(reducedBytes);
+            }
+  }
+  // https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings
+  public static string StringGenerator(string chars, int length)
+  {
+    StringBuilder res = new StringBuilder();
+    Random rnd = new Random();
+    while ( 0 < length--){
+        res.Append(chars[rnd.Next(chars.Length)]);
+    }
+    return res.ToString();
+  }
+  public static bool Compare(string h1, string h2, int bytes)
+  {
+    byte[] h1Bytes = Encoding.UTF8.GetBytes(h1);
+    byte[] h2Bytes = Encoding.UTF8.GetBytes(h2);
+    for(int i = 0; i < bytes; i++)
     {
-        byte[] newArray = new byte[byteArray.Length + 1];
-        byteArray.CopyTo(newArray, 1);
-        newArray[0] = newByte;
-        return newArray;
+        if(h1Bytes[i] != h2Bytes[i])
+        {
+            return false;
+        }
     }
-    public static byte[] MD5Hash(String text, byte salt)
+    return true;
+  }
+  public static string BirthdayAttack(string[] args)
+  {
+    Dictionary<string, string> hashTable = new Dictionary<string, string>();
+    while(true)
     {
-    // Use input string to calculate MD5 hash
-    using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-    {
-        byte[] byteArray = Encoding.UTF8.GetBytes(text);
-        byteArray = addByteToArray(byteArray, salt);
-        byte[] hashBytes = md5.ComputeHash(byteArray);
-        return hashBytes;
-
-
+        string salt = args[0];
+        const string validChars = "abcdefghijqlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int length = 10;
+        int bytes = 5;
+        string s1 = StringGenerator(validChars,length);
+        string hash = CreateMD5(s1, salt, bytes);
+        if(!hashTable.ContainsKey(hash))
+        {
+            hashTable.Add(hash, s1);
+        }
+        else{
+            if(s1 != hashTable[hash])
+            {
+                Console.WriteLine(s1 +","+hashTable[hash]);
+                return s1 + "," + hashTable[hash];
+            }
+        }
     }
-    }
+  }
+  public static void Main(String[] args){
+    BirthdayAttack(args);
+  }
 
-    static void Main(string[] args)
-    { 
-        var salt = Convert.ToByte(args[0]);
-        var text = "Hello World!";
-
-        Console.WriteLine(MD5Hash(text,salt));
-    }
 }
