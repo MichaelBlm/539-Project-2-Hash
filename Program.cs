66c2978b203bf57b0f29bf732a73ce3105bcf275
MichaelBlm <MichaelBlm94@gmail.com>
2023-04-01 22:21:34 -0700

    pushing updates

--- Program.cs
+++ Program.cs
@@ -1,35 +1,37 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Collections.Generic;
+using System.Text;
 namespace HashProject;
 class Program
 {
 
-public static string CreateMD5(string input, string salt)
-{
+    public static byte[] addByteToArray(byte[] byteArray, byte newByte)
+    {
+        byte[] newArray = new byte[byteArray.Length + 1];
+        byteArray.CopyTo(newArray, 1);
+        newArray[0] = newByte;
+        return newArray;
+    }
+    public static byte[] MD5Hash(String text, byte salt)
+    {
     // Use input string to calculate MD5 hash
     using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
     {
-        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
-        byte saltByte = Convert.ToByte(salt,16);
-        byte[] saltedInput = new byte[inputBytes.Length + 1];
-        for(int i = 0; i < inputBytes.Length; i++)
-        {
-          saltedInput[i] = inputBytes[i];
-        }
-        saltedInput[saltedInput.Length-1] = saltByte;
-        byte[] hashBytes = md5.ComputeHash(saltedInput);
-        Console.WriteLine(BitConverter.ToString(saltedInput).Replace("-"," "));
+        byte[] byteArray = Encoding.UTF8.GetBytes(text);
+        byteArray = addByteToArray(byteArray, salt);
+        byte[] hashBytes = md5.ComputeHash(byteArray);
+        return hashBytes;
 
-        // return Convert.ToHexString(hashBytes); // .NET 5 +
-        return BitConverter.ToString(hashBytes).Replace("-"," ");
+
     }
-}
+    }
+
     static void Main(string[] args)
     { 
-        var text = args[0];
-        var salt = args[1];
-        var output = CreateMD5(text, salt);
-        Console.WriteLine(output);
+        var salt = Convert.ToByte(args[0]);
+        var text = "Hello World!";
+
+        Console.WriteLine(MD5Hash(text,salt));
     }
 }
